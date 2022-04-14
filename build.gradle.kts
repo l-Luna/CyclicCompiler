4595ff0ca7aa684c27e443ef0fd14bae0a410031
Luna <izzeldeen03@gmail.com>
2022-04-14 01:04:31 +0100

    bump dependency versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,25 @@
-plugins {
+plugins{
     java
 }
 
 group = "com.github.l-Luna.CyclicCompiler"
 version = "0.0.2"
 
-repositories {
+repositories{
     mavenCentral()
 }
 
-dependencies {
+dependencies{
     implementation("org.antlr:antlr4-runtime:4.9.3")
-    implementation("org.ow2.asm:asm:9.2")
-    implementation("org.jetbrains:annotations:22.0.0")
+    implementation("org.ow2.asm:asm:9.3")
+    implementation("org.jetbrains:annotations:23.0.0")
     implementation("org.yaml:snakeyaml:1.30")
     
-	testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+	testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
-tasks.getByName<Test>("test") {
+tasks.getByName<Test>("test"){
     useJUnitPlatform()
     jvmArgs("--enable-preview")
 }
@@ -40,7 +40,7 @@ tasks.withType<Javadoc>{
     jdOptions.addBooleanOption("-enable-preview", true)
 }
 
-tasks.withType<Jar> {
+tasks.withType<Jar>{
     manifest {
         attributes["Main-Class"] = "cyclic.lang.compiler.CompilerLauncher"
     }
